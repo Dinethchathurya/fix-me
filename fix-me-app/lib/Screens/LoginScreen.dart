84908c2c4c15a8563c8944e02f6b5cd8a30ebdfb
Nlokuman998 <nironelokumannage234@gmail.com>
2024-03-07 14:33:52 +0530

    Update login.dart
    Created an email and password text input field.

--- fix-me-app/lib/Screens/LoginScreen.dart
+++ fix-me-app/lib/Screens/LoginScreen.dart
@@ -1,19 +1,82 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
   @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text('LOGIN page'),
-        //TODO crete logging page here
-
-        //TODO user can logging using email and password
-        //TODO he can get register page using register button
-
-        // TODO when user register successfully he needs to be redirect map page
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Login",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "Email",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Enter your email",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Password",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Enter your password",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       ),
     );
   }
