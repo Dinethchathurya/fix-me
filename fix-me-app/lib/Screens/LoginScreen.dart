1f54039a309ce90c661d5f036e5f8230c89edffd
Nlokuman998 <nironelokumannage234@gmail.com>
2024-03-07 15:06:06 +0530

    Password visibility button (Login.dart)
    Created a password visibility button using the 'suffixIcon' property. The 'setState' in the 'onPressed' method is used to toggle between the state of visibility.

--- fix-me-app/lib/Screens/LoginScreen.dart
+++ fix-me-app/lib/Screens/LoginScreen.dart
@@ -9,6 +9,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool _isObscuredText = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,119 +18,131 @@ class _LoginState extends State<Login> {
       ),
       body: Padding(
         padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            Text(
-              "Login",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Text(
-              "Email",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: "Enter your email",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "Password",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: "Enter your password",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Center(
-              child: SizedBox(
-                width: 200,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
                 height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // The 'onPressed' method is used to add the functionality to the button.
-                  },
-                  child: Text(
-                    "Log In",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                    ),
+              ),
+              Text(
+                "Login",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Email",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Enter your email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                      Color(0xFF39ACE7),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Password",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                  obscureText: _isObscuredText,
+                  decoration: InputDecoration(
+                    labelText: "Enter your password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(_isObscuredText
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      onPressed: () {
+                        setState(() {
+                          _isObscuredText = !_isObscuredText;
+                        });
+                      },
+                    ),
+                  )),
+              SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // The 'onPressed' method is used to add the functionality to the button.
+                    },
+                    child: Text(
+                      "Log In",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        Color(0xFF39ACE7),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't have an account ?",
-                  style: TextStyle(
-                    fontSize: 15,
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Register()));
-                  },
-                  child: Text(
-                    "Sign Up",
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account ?",
                     style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(
+                    width: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Register()));
+                    },
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
