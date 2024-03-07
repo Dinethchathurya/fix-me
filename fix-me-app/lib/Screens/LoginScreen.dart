a89ce25200a9054b6b870126cbda4e47cca8cc96
Nlokuman998 <nironelokumannage234@gmail.com>
2024-03-07 16:22:14 +0530

    Validation Logic
    Added validation logic to the email and password text form fields in the 'Login' page.

--- fix-me-app/lib/Screens/LoginScreen.dart
+++ fix-me-app/lib/Screens/LoginScreen.dart
@@ -9,7 +9,11 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
   bool _isObscuredText = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,129 +23,160 @@ class _LoginState extends State<Login> {
       body: Padding(
         padding: EdgeInsets.all(20.0),
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                "Login",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Text(
-                "Email",
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Enter your email",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
+                Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Text(
-                "Password",
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.normal,
+                SizedBox(
+                  height: 40,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                  obscureText: _isObscuredText,
+                Text(
+                  "Email",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: emailController,
                   decoration: InputDecoration(
-                    labelText: "Enter your password",
+                    labelText: "Enter your email",
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(20.0),
                     ),
-                    suffixIcon: IconButton(
-                      icon: Icon(_isObscuredText
-                          ? Icons.visibility_off
-                          : Icons.visibility),
-                      onPressed: () {
-                        setState(() {
-                          _isObscuredText = !_isObscuredText;
-                        });
-                      },
-                    ),
-                  )),
-              SizedBox(
-                height: 50,
-              ),
-              Center(
-                child: SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // The 'onPressed' method is used to add the functionality to the button.
-                    },
-                    child: Text(
-                      "Log In",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Email is missing";
+                    }
+                    String emailRegExp = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
+                    RegExp regexp = RegExp(emailRegExp);
+                    if (!regexp.hasMatch(value)) {
+                      return "Enter a valid email";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  "Password",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                    controller: passwordController,
+                    obscureText: _isObscuredText,
+                    decoration: InputDecoration(
+                      labelText: "Enter your password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(_isObscuredText
+                            ? Icons.visibility_off
+                            : Icons.visibility),
+                        onPressed: () {
+                          setState(() {
+                            _isObscuredText = !_isObscuredText;
+                          });
+                        },
                       ),
                     ),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                        Color(0xFF39ACE7),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Password is missing";
+                      }
+                      if (value.length < 8) {
+                        return "Password should be a minimum of eight characters";
+                      }
+                      return null;
+                    }),
+                SizedBox(
+                  height: 50,
+                ),
+                Center(
+                  child: SizedBox(
+                    width: 200,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // The 'onPressed' method is used to add the functionality to the button.
+                        if (_formKey.currentState!.validate()) {
+                          String email = emailController.text;
+                          String password = passwordController.text;
+                        }
+                      },
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Color(0xFF39ACE7),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account ?",
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Register()));
-                    },
-                    child: Text(
-                      "Sign Up",
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account ?",
                       style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    SizedBox(
+                      width: 10,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Register()));
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
