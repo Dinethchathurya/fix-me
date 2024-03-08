df47f01ba9f7a898b6b2c84a8202e7dce6d027af
Nlokuman998 <nironelokumannage234@gmail.com>
2024-03-08 20:57:02 +0530

    Create AuthPage.dart
    Uses a stream builder to check for any state changes when the user logs in.

---
+++ fix-me-app/lib/Screens/AuthPage.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fix_me_app/Screens/LoginScreen.dart';
+import 'package:fix_me_app/Screens/MapScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return MapScreen();
+              } else {
+                return Login();
+              }
+            }));
+  }
+}
